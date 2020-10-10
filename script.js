05536478057473daf0cc450f54cdfb65a6bfa509
PRAVEEN S <praveenselvaraj27@gmail.com>
2020-10-10 12:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Wait till the DOM is ready.
 $(function () {
-  var roles = ["Artist", "Student", "VITian"];
+  var roles = ["Innovator", "Student", "Realist"];
   var count = 0;
   var $typeSpan = $("#intro .content h2 span").typist({
     text: roles[count]
